a1c92d32dad03f423021136d57a382e3141dc6fd
tob1tbs <chikhladze.mt@gmail.com>
2023-06-14 17:14:57 +0400

    Update layout.blade.php

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -79,5 +79,13 @@
 	        }
 	    }
 	})
+	$('.owl-carousel').on('mousewheel', '.owl-stage', function (e) {
+	    if (e.deltaY>0) {
+	        owl.trigger('next.owl');
+	    } else {
+	        owl.trigger('prev.owl');
+	    }
+	    e.preventDefault();
+	});
 </script>
 </html>
