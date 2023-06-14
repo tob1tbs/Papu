117145aed5cf82250be260af1b0fd0abc49e9094
tob1tbs <chikhladze.mt@gmail.com>
2023-06-14 17:37:36 +0400

    Update main.blade.php

--- resources/views/web/main.blade.php
+++ resources/views/web/main.blade.php
@@ -1,27 +1,29 @@
 @extends('web.layout')
 
 @section('content')
-<div class="container" style="max-width: 100%; margin-top: 10px;">
-	<div class="owl-carousel">
-		<div>
-			<a href="{{ route('actionWebGames') }}?hash={{ request()->hash }}">
-				<img src="{{ asset('assets/images/games.png')}}" alt="">
-			</a>
-		</div>
-		<div>
-			<a href="https://web.unitear.com/">
-				<img src="{{ asset('assets/images/reality.png')}}" alt="">
-			</a>
-		</div>
-		<div>
-			<a href="{{ route('actionWebBook') }}?hash={{ request()->hash }}">
-				<img src="{{ asset('assets/images/store.png')}}" alt="">
-			</a>
-		</div>
-		<div>
-			<a href="https://events.getsnappic.com/virtual/capture/g5w3j?">
-				<img src="{{ asset('assets/images/photo.png')}}" alt="">
-			</a>
+<div class="container" style="max-width: 100%;">
+	<div>
+		<div class="owl-carousel">
+			<div>
+				<a href="{{ route('actionWebGames') }}?hash={{ request()->hash }}">
+					<img src="{{ asset('assets/images/games.png')}}" alt="">
+				</a>
+			</div>
+			<div>
+				<a href="https://web.unitear.com/">
+					<img src="{{ asset('assets/images/reality.png')}}" alt="">
+				</a>
+			</div>
+			<div>
+				<a href="{{ route('actionWebBook') }}?hash={{ request()->hash }}">
+					<img src="{{ asset('assets/images/store.png')}}" alt="">
+				</a>
+			</div>
+			<div>
+				<a href="https://events.getsnappic.com/virtual/capture/g5w3j?">
+					<img src="{{ asset('assets/images/photo.png')}}" alt="">
+				</a>
+			</div>
 		</div>
 	</div>
 </div>
