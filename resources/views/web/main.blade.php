0cda7d42d8c90d066fcca7fc6e9be19e00c53180
tob1tbs <chikhladze.mt@gmail.com>
2023-06-14 18:35:12 +0400

    Update main.blade.php

--- resources/views/web/main.blade.php
+++ resources/views/web/main.blade.php
@@ -2,26 +2,26 @@
 
 @section('content')
 <div class="container" style="max-width: 100%;">
-	<div style="height: calc(100vh - 90px); display: flex; align-items: center;">
+	<div>
 		<div class="owl-carousel">
 			<div>
 				<a href="{{ route('actionWebGames') }}?hash={{ request()->hash }}">
-					<img src="{{ asset('assets/images/games.png')}}" alt="">
+					<img src="{{ asset('assets/images/games.png')}}" alt="" style="height: calc(100vh - 120px);">
 				</a>
 			</div>
 			<div>
 				<a href="https://web.unitear.com/">
-					<img src="{{ asset('assets/images/reality.png')}}" alt="">
+					<img src="{{ asset('assets/images/reality.png')}}" alt="" style="height: calc(100vh - 120px);">
 				</a>
 			</div>
 			<div>
 				<a href="{{ route('actionWebBook') }}?hash={{ request()->hash }}">
-					<img src="{{ asset('assets/images/store.png')}}" alt="">
+					<img src="{{ asset('assets/images/store.png')}}" alt="" style="height: calc(100vh - 120px);">
 				</a>
 			</div>
 			<div>
 				<a href="https://events.getsnappic.com/virtual/capture/g5w3j?">
-					<img src="{{ asset('assets/images/photo.png')}}" alt="">
+					<img src="{{ asset('assets/images/photo.png')}}" alt="" style="height: calc(100vh - 120px);">
 				</a>
 			</div>
 		</div>
