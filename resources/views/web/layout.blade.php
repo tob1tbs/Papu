7de98c002f2cd53ead1c4461177692611dfdb521
tob1tbs <chikhladze.mt@gmail.com>
2023-06-14 17:47:28 +0400

    Update layout.blade.php

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -51,6 +51,7 @@
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="{{ asset('assets/owlcarousel/owl.carousel.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
 <script>
 	var owl = $('.owl-carousel');
 	owl.owlCarousel({
